958620d4868f02427fe12a53bee530f4053aa407
Jack Cutting <me@jackcutting.co.uk>
2017-01-11 18:36:13 +0100

    Add custom validation messages

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -1,4 +1,5 @@
 # Chirps Model
 class Chirp < ApplicationRecord
-  validates :body, presence: true, length: { maximum: 150 }
+  validates :body, presence: { message: 'Chirp cannot be empty!' }, 
+    length: { maximum: 150, too_long: "Chirp cannot be more than %{count} characters." }
 end
