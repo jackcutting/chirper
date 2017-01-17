6db211709c178ede04c43bd0385979bd099834b0
Jack Cutting <me@jackcutting.co.uk>
2017-01-17 12:27:47 +0000

    Remove unnecessary column

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@
 ActiveRecord::Schema.define(version: 201_701_022_021_30) do
   create_table 'chirps', force: :cascade do |t|
     t.text     'body'
-    t.string   'author'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
