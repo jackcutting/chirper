143f258c944b8afaedd07914e3320e31c055f1e5
Jack Cutting <me@jackcutting.co.uk>
2017-01-17 12:58:58 +0000

    Update testing methods

--- spec/features/chirps_model_spec.rb
+++ spec/features/chirps_model_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 require 'factory_girl'
 require 'ffaker'
 
-describe Chirp do
+describe Chirp, type: :model do
   it 'has a valid factory' do
     expect(create(:chirp)).to be_valid
   end
 
-  it 'is invalid without body' do
-    expect(build(:chirp, body: nil)).not_to be_valid
+  it 'is invalid with a body greater than 150 characters' do
+    should validate_presence_of(:body).with_message(/cannot be empty/)
   end
 
   it 'is invalid with a body greater than 150 characters' do
-    expect(build(:chirp, body: FFaker::Lorem.paragraphs)).not_to be_valid
+    should validate_length_of(:body).is_at_most(150)
+      .with_message(/cannot be more than 150 characters/)
   end
 
   it 'returns a Chirp\'s body as a string' do
